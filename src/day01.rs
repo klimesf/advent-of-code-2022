a45088af3458d65c0fdba761a02e44842831e741
Filip Klimeš <filip@filipklimes.cz>
2022-12-01 08:04:37 +0100

    day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,13 +1,36 @@
-use std::fs;
+use std::collections::BinaryHeap;
+use crate::io::read_lines;
 
 pub(crate) fn day01() {
-    let input = fs::read_to_string("input/day01/test.txt").unwrap();
+    let mut elves = BinaryHeap::new();
+    let mut current_elf: i32 = 0;
+
+    if let Ok(lines) = read_lines("input/day01/input.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip.len() == 0 {
+                    elves.push(current_elf);
+                    current_elf = 0;
+                } else {
+                    current_elf += ip.parse::<i32>().unwrap();
+                }
+            }
+        }
+        elves.push(current_elf)
+    }
+
+    part_a(&elves);
+    part_b(&mut elves);
 }
 
-#[cfg(test)]
-mod day01_tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+fn part_a(elves: &BinaryHeap<i32>) {
+    println!("{}", elves.peek().unwrap())
+}
+
+fn part_b(elves: &mut BinaryHeap<i32>) {
+    let max1 = elves.pop().unwrap();
+    let max2 = elves.pop().unwrap();
+    let max3 = elves.pop().unwrap();
+
+    println!("{} + {} + {} = {}", max1, max2, max3, max1 + max2 + max3)
 }
