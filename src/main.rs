fecf7497855c3e45883e24524151713d34a503eb
Filip Klimeš <filip@filipklimes.cz>
2022-12-17 18:36:32 +0100

    colorize & measure output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate itertools;
 extern crate regex;
 
+use colored::Colorize;
 use std::env;
 use crate::day01::day01;
 use crate::day02::day02;
@@ -40,63 +41,71 @@ mod day16;
 mod day17;
 mod toolbox;
 
+macro_rules! measure {
+    ($s:stmt) => {
+        let timer = std::time::Instant::now();
+        $s
+        println!("{}", format!("Elapsed: {:?}", timer.elapsed()).italic().dimmed());
+    };
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.contains(&"all".to_string()) {
         println!();
-        println!("Advent of Code");
-        println!("        //2022");
+        println!("{}", format!("Advent of Code").red());
+        println!("{}", format!("        //2022").red());
         println!();
 
-        println!("--- day01:");
-        day01();
+        println!("{}", format!("--- day01:").underline().green());
+        measure!(day01());
 
-        println!("--- day02:");
-        day02();
+        println!("{}", format!("--- day02:").underline().green());
+        measure!(day02());
 
-        println!("--- day03:");
-        day03();
+        println!("{}", format!("--- day03:").underline().green());
+        measure!(day03());
 
-        println!("--- day04:");
-        day04();
+        println!("{}", format!("--- day04:").underline().green());
+        measure!(day04());
 
-        println!("--- day05:");
-        day05();
+        println!("{}", format!("--- day05:").underline().green());
+        measure!(day05());
 
-        println!("--- day06:");
-        day06();
+        println!("{}", format!("--- day06:").underline().green());
+        measure!(day06());
 
-        println!("--- day07:");
-        day07();
+        println!("{}", format!("--- day07:").underline().green());
+        measure!(day07());
 
-        println!("--- day08:");
-        day08();
+        println!("{}", format!("--- day08:").underline().green());
+        measure!(day08());
 
-        println!("--- day09:");
-        day09();
+        println!("{}", format!("--- day09:").underline().green());
+        measure!(day09());
 
-        println!("--- day10:");
-        day10();
+        println!("{}", format!("--- day10:").underline().green());
+        measure!(day10());
 
-        println!("--- day11:");
-        day11();
+        println!("{}", format!("--- day11:").underline().green());
+        measure!(day11());
 
-        println!("--- day12:");
-        day12();
+        println!("{}", format!("--- day12:").underline().green());
+        measure!(day12());
 
-        println!("--- day13:");
-        day13();
+        println!("{}", format!("--- day13:").underline().green());
+        measure!(day13());
 
-        println!("--- day14:");
-        day14();
+        println!("{}", format!("--- day14:").underline().green());
+        measure!(day14());
 
-        println!("--- day15:");
-        day15();
+        println!("{}", format!("--- day15:").underline().green());
+        measure!(day15());
 
-        println!("--- day16:");
-        day16();
+        println!("{}", format!("--- day16:").underline().green());
+        measure!(day16());
     }
 
-    println!("--- day17:");
-    day17();
+    println!("{}", format!("--- day17:").underline().green());
+    measure!(day17());
 }
