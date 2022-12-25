608cc61342f8b82c3ed54bb3b57d40e9a7c42808
Filip Klimeš <filip@filipklimes.cz>
2022-12-25 11:07:46 +0100

    day19 otpimizations

--- src/day19.rs
+++ src/day19.rs
@@ -21,15 +21,15 @@ pub(crate) fn day19() {
 
     let ans = blueprints.par_iter()
         .map(|(id, ore_cost, clay_cost, obsidian_cost, geode_cost)|
-            part_a(*ore_cost, *clay_cost, *obsidian_cost, *geode_cost) * id)
+            solve(24, *ore_cost, *clay_cost, *obsidian_cost, *geode_cost) * id)
         .sum::<i32>();
     println!("{}", ans);
 
     let ans = blueprints.iter()
         .take(3)
         .par_bridge()
-        .map(|(id, ore_cost, clay_cost, obsidian_cost, geode_cost)|
-            part_b(*id, *ore_cost, *clay_cost, *obsidian_cost, *geode_cost))
+        .map(|(_, ore_cost, clay_cost, obsidian_cost, geode_cost)|
+            solve(32, *ore_cost, *clay_cost, *obsidian_cost, *geode_cost))
         .product::<i32>();
     println!("{}", ans);
 }
@@ -40,135 +40,112 @@ fn parse_i32(g: Option<Match>) -> i32 {
 
 type Cost = (i32, i32, i32);
 
-#[allow(dead_code)]
-fn part_a(ore_cost: Cost, clay_cost: Cost, obsidian_cost: Cost, geode_cost: Cost) -> i32 {
+fn solve(time: i32, ore_cost: Cost, clay_cost: Cost, obsidian_cost: Cost, geode_cost: Cost) -> i32 {
     let robot_ctr = [1, 0, 0, 0];
     let resource_ctr = [0; 4];
-    let robot_factory = [0, 0, 0, 0];
     let max_ore_cost = vec!(ore_cost.0, clay_cost.0, obsidian_cost.0, geode_cost.0).into_iter().max().unwrap();
     let max_clay_cost = vec!(ore_cost.1, clay_cost.1, obsidian_cost.1, geode_cost.1).into_iter().max().unwrap();
     let mut states = vec!();
-    states.push((0, robot_ctr.clone(), resource_ctr.clone(), robot_factory.clone()));
+    states.push((0, robot_ctr.clone(), resource_ctr.clone()));
 
-    let mut max = i32::MIN;
+    let mut max = 0;
     while !states.is_empty() {
-        let (minute, mut robot_ctr, mut resource_ctr, mut robot_factory) = states.pop().unwrap();
-        if minute == 24 {
+        let (minute,robot_ctr, resource_ctr) = states.pop().unwrap();
+        if minute == time {
             if resource_ctr[3] > max {
                 max = resource_ctr[3];
             }
             continue;
         }
 
-        let available_res = resource_ctr.clone();
-        for i in 0..4 {
-            robot_ctr[i] += robot_factory[i];
-            robot_factory[i] = 0;
+        let mut possible_max = resource_ctr[3] + (robot_ctr[3] * (time - minute));
+        for i in 0..time - minute {
+            possible_max += i;
         }
-        for i in 0..4 { resource_ctr[i] += robot_ctr[i]; }
-        if (24 - minute) * robot_ctr[3].max(1) * 2 < max - resource_ctr[3] {
-            continue;
+        if possible_max <= max {
+            continue; // Prune
         }
 
-        if geode_cost.0 <= available_res[0] && geode_cost.1 <= available_res[1] && geode_cost.2 <= available_res[2] {
-            let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= geode_cost.0;
-            res_clone[1] -= geode_cost.1;
-            res_clone[2] -= geode_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [0, 0, 0, 1]));
-            continue; // Greedy - if you can build a geode robot, just do it
-        }
+        let mut branch_count = 0;
 
-        states.push((minute + 1, robot_ctr.clone(), resource_ctr.clone(), [0, 0, 0, 0]));
-        if robot_ctr[0] < max_ore_cost && ore_cost.0 <= available_res[0] && ore_cost.1 <= available_res[1] && ore_cost.2 <= available_res[2] {
-            let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= ore_cost.0;
-            res_clone[1] -= ore_cost.1;
-            res_clone[2] -= ore_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [1, 0, 0, 0]));
-        }
-        if robot_ctr[1] < max_clay_cost && clay_cost.0 <= available_res[0] && clay_cost.1 <= available_res[1] && clay_cost.2 <= available_res[2] {
-            let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= clay_cost.0;
-            res_clone[1] -= clay_cost.1;
-            res_clone[2] -= clay_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [0, 1, 0, 0]));
-        }
-        if obsidian_cost.0 <= available_res[0] && obsidian_cost.1 <= available_res[1] && obsidian_cost.2 <= available_res[2] {
-            let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= obsidian_cost.0;
-            res_clone[1] -= obsidian_cost.1;
-            res_clone[2] -= obsidian_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [0, 0, 1, 0]));
-        }
-    }
-    return max;
-}
-
-#[allow(dead_code)]
-fn part_b(id: i32, ore_cost: Cost, clay_cost: Cost, obsidian_cost: Cost, geode_cost: Cost) -> i32 {
-    let robot_ctr = [1, 0, 0, 0];
-    let resource_ctr = [0; 4];
-    let robot_factory = [0, 0, 0, 0];
-    let max_ore_cost = vec!(ore_cost.0, clay_cost.0, obsidian_cost.0, geode_cost.0).into_iter().max().unwrap();
-    let max_clay_cost = vec!(ore_cost.1, clay_cost.1, obsidian_cost.1, geode_cost.1).into_iter().max().unwrap();
-    let mut states = vec!();
-    states.push((0, robot_ctr.clone(), resource_ctr.clone(), robot_factory.clone()));
-
-    let mut max = i32::MIN;
-    while !states.is_empty() {
-        let state = states.pop().unwrap();
-        let (minute, mut robot_ctr, mut resource_ctr, mut robot_factory) = state;
-        if minute == 32 {
-            if resource_ctr[3] > max {
-                max = resource_ctr[3];
-                println!("blueprint {}: {:?}, robots {:?}", id, resource_ctr, robot_ctr);
+        // Build ore bot next
+        if robot_ctr[0] < max_ore_cost {
+            let ore_available_in = ((ore_cost.0 - resource_ctr[0] + robot_ctr[0] - 1) / robot_ctr[0]).max(0) + 1;
+            if minute + ore_available_in <= time {
+                branch_count += 1;
+                let mut res_clone = resource_ctr.clone();
+                res_clone[0] += (robot_ctr[0] * ore_available_in) - ore_cost.0;
+                res_clone[1] += (robot_ctr[1] * ore_available_in) - ore_cost.1;
+                res_clone[2] += (robot_ctr[2] * ore_available_in) - ore_cost.2;
+                res_clone[3] += robot_ctr[3] * ore_available_in;
+                let mut robot_ctr_clone = robot_ctr.clone();
+                robot_ctr_clone[0] += 1;
+                states.push((minute + ore_available_in, robot_ctr_clone, res_clone));
             }
-            continue;
         }
 
-        let available_res = resource_ctr.clone();
-        for i in 0..4 {
-            robot_ctr[i] += robot_factory[i];
-            robot_factory[i] = 0;
-        }
-        for i in 0..4 { resource_ctr[i] += robot_ctr[i]; }
-        if (32 - minute) * robot_ctr[3].max(1) * 5 < max - resource_ctr[3] {
-            continue;
-        }
-
-        if geode_cost.0 <= available_res[0] && geode_cost.1 <= available_res[1] && geode_cost.2 <= available_res[2] {
-            let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= geode_cost.0;
-            res_clone[1] -= geode_cost.1;
-            res_clone[2] -= geode_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [0, 0, 0, 1]));
-            continue; // Greedy - if you can build a geode robot, just do it
+        // Build clay bot next
+        if robot_ctr[1] < max_clay_cost {
+            let ore_available_in = ((clay_cost.0 - resource_ctr[0] + robot_ctr[0] - 1) / robot_ctr[0]).max(0) + 1;
+            if minute + ore_available_in <= time {
+                branch_count += 1;
+                let mut res_clone = resource_ctr.clone();
+                res_clone[0] += (robot_ctr[0] * ore_available_in) - clay_cost.0;
+                res_clone[1] += (robot_ctr[1] * ore_available_in) - clay_cost.1;
+                res_clone[2] += (robot_ctr[2] * ore_available_in) - clay_cost.2;
+                res_clone[3] += robot_ctr[3] * ore_available_in;
+                let mut robot_ctr_clone = robot_ctr.clone();
+                robot_ctr_clone[1] += 1;
+                states.push((minute + ore_available_in, robot_ctr_clone, res_clone));
+            }
         }
 
-        states.push((minute + 1, robot_ctr.clone(), resource_ctr.clone(), [0, 0, 0, 0]));
-        if robot_ctr[0] < max_ore_cost && ore_cost.0 <= available_res[0] && ore_cost.1 <= available_res[1] && ore_cost.2 <= available_res[2] {
-            let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= ore_cost.0;
-            res_clone[1] -= ore_cost.1;
-            res_clone[2] -= ore_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [1, 0, 0, 0]));
+        // Build obsidian bot next
+        if robot_ctr[0] > 0 && robot_ctr[1] > 0 {
+            let ore_available_in = ((obsidian_cost.0 - resource_ctr[0] + robot_ctr[0] - 1) / robot_ctr[0]).max(0) + 1;
+            let clay_available_in = ((obsidian_cost.1 - resource_ctr[1] + robot_ctr[1] - 1) / robot_ctr[1]).max(0) + 1;
+            let robot_available_in = ore_available_in.max(clay_available_in);
+            if minute + robot_available_in <= time {
+                branch_count += 1;
+                let mut res_clone = resource_ctr.clone();
+                res_clone[0] += (robot_ctr[0] * robot_available_in) - obsidian_cost.0;
+                res_clone[1] += (robot_ctr[1] * robot_available_in) - obsidian_cost.1;
+                res_clone[2] += (robot_ctr[2] * robot_available_in) - obsidian_cost.2;
+                res_clone[3] += robot_ctr[3] * robot_available_in;
+                let mut robot_ctr_clone = robot_ctr.clone();
+                robot_ctr_clone[2] += 1;
+                states.push((minute + robot_available_in, robot_ctr_clone, res_clone));
+            }
         }
-        if robot_ctr[1] < max_clay_cost && clay_cost.0 <= available_res[0] && clay_cost.1 <= available_res[1] && clay_cost.2 <= available_res[2] {
-            let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= clay_cost.0;
-            res_clone[1] -= clay_cost.1;
-            res_clone[2] -= clay_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [0, 1, 0, 0]));
+
+        // Build geode bot next
+        if robot_ctr[0] > 0 && robot_ctr[2] > 0 {
+            let ore_available_in = ((geode_cost.0 - resource_ctr[0] + robot_ctr[0] - 1) / robot_ctr[0]).max(0) + 1;
+            let obsidian_available_in = ((geode_cost.2 - resource_ctr[2] + robot_ctr[2] - 1) / robot_ctr[2]).max(0) + 1;
+            let robot_available_in = ore_available_in.max(obsidian_available_in);
+            if minute + robot_available_in <= time {
+                branch_count += 1;
+                let mut res_clone = resource_ctr.clone();
+                res_clone[0] += (robot_ctr[0] * robot_available_in) - geode_cost.0;
+                res_clone[1] += (robot_ctr[1] * robot_available_in) - geode_cost.1;
+                res_clone[2] += (robot_ctr[2] * robot_available_in) - geode_cost.2;
+                res_clone[3] += robot_ctr[3] * robot_available_in;
+                let mut robot_ctr_clone = robot_ctr.clone();
+                robot_ctr_clone[3] += 1;
+                states.push((minute + robot_available_in, robot_ctr_clone, res_clone));
+            }
         }
-        if obsidian_cost.0 <= available_res[0] && obsidian_cost.1 <= available_res[1] && obsidian_cost.2 <= available_res[2] {
+
+        if branch_count == 0 {
+            // No time to build other robots, simulate to minute 30
+            let rem = time - minute - 1;
             let mut res_clone = resource_ctr.clone();
-            res_clone[0] -= obsidian_cost.0;
-            res_clone[1] -= obsidian_cost.1;
-            res_clone[2] -= obsidian_cost.2;
-            states.push((minute + 1, robot_ctr.clone(), res_clone, [0, 0, 1, 0]));
+            res_clone[0] += resource_ctr[0] * rem;
+            res_clone[1] += resource_ctr[1] * rem;
+            res_clone[2] += resource_ctr[2] * rem;
+            res_clone[3] += resource_ctr[3] * rem;
+            states.push((time, robot_ctr.clone(), res_clone));
         }
     }
-    println!("blueprint {} finished! ans: {}", id, max);
     return max;
 }
